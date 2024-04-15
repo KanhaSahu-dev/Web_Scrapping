e50d0f61d477560f3974b8eab1825358902de379
Kanha <kanhasahu273273@gmail.com>
2024-04-15 13:05:55 +0530

    Final Update to the filter_new python file and applied the logic to filter the best products

--- filter_new.py
+++ filter_new.py
@@ -8,16 +8,16 @@ df['Price'] = df['Price'].str.replace('Rs ', '').str.replace(',', '').astype(flo
 
 # Calculate means
 Customers_Rated_mean = df["Customers_Rated"].mean()
-Price_mean = df["Price"].mean()
+Price_mean = df["Price"].mean() / 2
 
-# Filter the dataframe
-filtered_df = df[(df["Customers_Rated"] >= Customers_Rated_mean) & (df["Price"] >= Price_mean)]
+# Apply filter
+Best_product = df[(df["Customers_Rated"] >= Customers_Rated_mean) & (df["Price"] >= Price_mean)]
 
 # Sort the filtered dataframe based on multiple criteria
-sorted_df = filtered_df.sort_values(by=['Customers_Rated', 'Price', 'Rating'], ascending=[False, False, False])
+sorted_df = Best_product.sort_values(by=['Customers_Rated', 'Price', 'Rating'], ascending=[False, False, False])
 
 # Concatenate the filtered and sorted dataframe with the remaining data
-final_df = pd.concat([sorted_df, df[~df.index.isin(filtered_df.index)]])
+final_df = pd.concat([sorted_df, df[~df.index.isin(Best_product.index)]])
 
 # Save the final dataframe back to CSV
-final_df.to_csv('amazon_products_sorted.csv', index=False)
+final_df.to_csv('amazon_products.csv', index=False)
